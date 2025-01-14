2a92763342127a834a736ca863a1fba4e76b2e85
austindodig <dodigaj@gmail.com>
2025-01-14 14:51:13 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -453,5 +453,5 @@ print(app.url_map)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='127.0.0.1', port=int(os.environ.get('PORT', 25002)), debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 25002)), debug=True)
 
